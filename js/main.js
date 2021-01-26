cc2d5cb74e8c7782e480cda31d87d26c558783a3
keiken-shin <anubhav.kumar27@gmail.com>
2021-01-26 13:37:58 +0530

    Updated CDN

--- js/main.js
+++ js/main.js
@@ -3,15 +3,27 @@ let toggler = false;
 let hamIcon = document.querySelector(".hamburger-icon");
 let nav = document.querySelector(".navbar");
 
-hamIcon.addEventListener('click', () => {
+try{
+    hamIcon.addEventListener('click', () => {
+    
+        if(toggler === false){
+            nav.style.visibility = "visible";
+            // nav.style.transition = 'max-height 1s ease-in';
+            toggler = true;
+        }else if(toggler === true){
+            nav.style.visibility = "hidden";
+            toggler = false;
+        }
+    
+    })
+}catch(err){}
 
-    if(toggler === false){
-        nav.style.visibility = "visible";
-        // nav.style.transition = 'max-height 1s ease-in';
-        toggler = true;
-    }else if(toggler === true){
-        nav.style.visibility = "hidden";
-        toggler = false;
-    }
-
-})
+function copyURI(e) {
+    e.preventDefault();
+    navigator.clipboard.writeText(e.target.getAttribute('href')).then(() => {
+      e.target.textContent = "Copied to clipboard"
+      setTimeout(() => e.target.textContent = "Get Cage CDN", 2000)
+    }, () => {
+      /* clipboard write failed */
+    });
+}
